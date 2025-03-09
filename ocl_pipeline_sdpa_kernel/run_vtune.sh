5de9613f2e4cd25a46fea9b8e9f1ab9159fafb9b
xipingya <xiping.yan@intel.com>
2025-03-09 20:49:14 +0800

    vtune script

--- ocl_pipeline_sdpa_kernel/run_vtune.sh
+++ ocl_pipeline_sdpa_kernel/run_vtune.sh
@@ -3,9 +3,9 @@ source /opt/intel/oneapi/setvars.sh
 export OV_DEVICE='GPU'
 
 # Test: model_gemm.py
-logs_dir=vtune_log_dir_ocl_pipeline_rope_kernel
+logs_dir=vd_ocl_sdpa_kernel
 rm -rf $logs_dir
 mkdir -p $logs_dir
 # OV_GPU_Verbose=4 ONEDNN_VERBOSE=1 
 
-PERFORMANCE=1 vtune -collect gpu-hotspots -r $logs_dir -knob gpu-sampling-interval=0.1 -- ./ocl_pipeline_rope_kernel
+PERFORMANCE=1 vtune -collect gpu-hotspots -r $logs_dir -knob gpu-sampling-interval=0.1 -- ./ocl_pipeline_sdpa_kernel
--- sycl_pipeline_sdpa_kernel/run_vtune.sh
+++ sycl_pipeline_sdpa_kernel/run_vtune.sh
@@ -3,9 +3,9 @@ source /opt/intel/oneapi/setvars.sh
 export OV_DEVICE='GPU'
 
 # Test: model_gemm.py
-logs_dir=vtune_log_dir_sycl_pipeline_rope_kernel
+logs_dir=vd_sycl_sdpa_kernel
 rm -rf $logs_dir
 mkdir -p $logs_dir
 # OV_GPU_Verbose=4 ONEDNN_VERBOSE=1 
 
-PERFORMANCE=1 vtune -collect gpu-hotspots -r $logs_dir -knob gpu-sampling-interval=0.1 -- ./sycl_pipeline_rope_kernel
+PERFORMANCE=1 vtune -collect gpu-hotspots -r $logs_dir -knob gpu-sampling-interval=0.1 -- ./sycl_pipeline_sdpa_kernel
